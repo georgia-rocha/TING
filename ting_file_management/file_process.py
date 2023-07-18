77a43a3dab0038cd3002057ed846cb7cd509e4e4
Georgia <georgiab543@gmail.com>
2023-07-17 23:53:28 -0300

    feat: requisitos 3, 4 e 5 - implementando funções para armazenar conteúdo da lista em um dic, dentro da queue, para remover o primeiro arq processado, uma para apresentar as infos superficiais de um arq processado e uma função complementar "create" para complementar as outras funções

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,39 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    new = create(path_file)
+
+    if path_file not in instance.queue:
+        instance.enqueue(path_file)
+        return print(new, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        return print("Não há elementos", file=sys.stdout)
+
+    path_file = instance.dequeue()
+    print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position < 0 or position > len(instance):
+        return print("Posição inválida", file=sys.stderr)
+
+    file_search = instance.search(position)
+    new = create(file_search)
+
+    print(new, file=sys.stdout)
+
+
+def create(path_file):
+    file = txt_importer(path_file)
+    new = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+
+    return new
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,20 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.fila = []
+        self.queue = []
 
     def __len__(self):
-        return len(self.fila)
+        return len(self.queue)
 
     def enqueue(self, value):
-        self.fila.append(value)
+        self.queue.append(value)
 
     def dequeue(self):
         if (self.__len__()):
-            return self.fila.pop(0)
+            return self.queue.pop(0)
 
     def search(self, index):
-        if (index >= 0 and index <= len(self.fila) - 1):
-            return self.fila[index]
+        if (index >= 0 and index <= len(self.queue) - 1):
+            return self.queue[index]
         else:
             raise IndexError("Índice Inválido ou Inexistente")
