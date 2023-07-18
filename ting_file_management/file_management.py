5e74a8f7830e67bb947760f31cd050e2a31cf3f0
Georgia <georgiab543@gmail.com>
2023-07-17 22:35:19 -0300

    feat: requisito 2 - Implementando uma função, capz de importar news a partir de um arquivo TXT, utilizando "\n" como separador

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file_name = os.path.basename(path_file)
+    ext = os.path.splitext(file_name)
+
+    if ext[1] != '.txt':
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
