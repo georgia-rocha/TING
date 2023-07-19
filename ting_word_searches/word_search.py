f619348b2b81d07c2e62a5ad8553f8e36f67ecec
Georgia <georgiab543@gmail.com>
2023-07-19 13:48:53 -0300

    feat: requisito 7 - Implementando função que verifica a existência de uma palavra em todos os arq processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,25 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+
+    if not len(instance):
+        return None
+
+    def find_word_in_lines(lines):
+        return [{"linha": i} for i, line in enumerate(lines, 1)
+                if word.lower() in line.lower()]
+
+    occurrences = [{
+        "palavra": word,
+        "arquivo": file,
+        "ocorrencias": find_word_in_lines(txt_importer(file))
+    } for file in instance.queue if
+        any(find_word_in_lines(txt_importer(file)))]
+
+    return occurrences
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    pass
